8d768c9d932936df7a1ff728fd1bcf328c2cd196
Kyle.chang <1210949654@qq.com>
2019-06-20 11:27:22 +0800

    no message

--- src/main/java/com/ch/dao/ClientCollectionRecordMapper.java
+++ src/main/java/com/ch/dao/ClientCollectionRecordMapper.java
@@ -2,8 +2,9 @@ package com.ch.dao;
 
 import com.ch.entity.ClientCollectionRecord;
 import com.ch.entity.ClientCollectionRecordExample;
-import java.util.List;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface ClientCollectionRecordMapper {
     long countByExample(ClientCollectionRecordExample example);
