189c2e35b685c1ec9c403e8a8b9041cd4f354c4e
changfuwen <1712710548@qq.com>
2019-07-23 10:21:06 +0800

    no message

--- src/test/java/com/ch/ch/ChApplicationTests.java
+++ src/test/java/com/ch/ch/ChApplicationTests.java
@@ -3,16 +3,15 @@
 //
 //import com.alibaba.fastjson.JSON;
 //import com.ch.base.ResponseResult;
-//import com.ch.dao.LookShopMapper;
-//import com.ch.dao.SysUserMapper;
-//import com.ch.dao.TransferShopMapper;
-//import com.ch.dao.WangMapper;
+//import com.ch.dao.*;
 //import com.ch.service.UploadService;
 //import org.junit.Test;
 //import org.junit.runner.RunWith;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.test.context.junit4.SpringRunner;
+//
+//import java.util.List;
 //
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
@@ -29,6 +28,8 @@
 //
 //    @Autowired
 //    UploadService uploadService;
+//    @Autowired
+//    CommerciaEstatMapper commerciaEstatMapper;
 //
 //
 //
@@ -41,7 +42,43 @@
 //
 //
 //
+//    @Test
+//    public void testUploadImag1e(){
+//        for (int i = 0; i <1200 ; i++) {
+//            int random=(int)(Math.random()*10+1);
+//            System.out.println(random);
+//        }
 //
+//
+//    }
+//
+//
+//    @Test
+//    public void testUploadImage2(){
+//        //ResponseResult result = uploadService.uploadAddress("/Users/changhan/Downloads/937626DDEC847DB3040A2E7D069F222A.jpg");
+//        //System.out.println(JSON.toJSON(result));
+//        List<String> category = commerciaEstatMapper.findCategory();
+//        for (String s : category) {
+//            if (s.length()>4){
+//                String[] split = s.split("、");
+//                String s1 = split[1];
+//                String trim = s1.trim();
+//                //System.out.println(trim);
+//                String substring = trim.substring(2, trim.length() - 1);
+//                double v = Float.parseFloat(substring);
+//
+//                int floor =(int) Math.floor(v);
+//                System.out.println(floor);
+//
+//
+//            }else {
+//                System.out.println(0);
+//            }
+//        }
+//
+//
+//
+//    }
 //}
-//
-//
+
+
